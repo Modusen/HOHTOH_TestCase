74f357c00fda144f9c6ad08ddf3e87e03b57174d
DmitriyAstrashenya <diman.lan@Mail.ru>
2023-05-31 11:22:35 +0300

    TesCase Done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ProductImpl productImpl = new ProductImpl();
+
+        Product product1 = new Product(1, "Sofa");
+        Product product2 = new Product(2, "Table");
+        Product product3 = new Product(3, "Chair");
+        Product product4 = new Product(4, "Bed");
+        Product product5 = new Product(5, "Chair");
+        productImpl.productsList.add(product1);
+        productImpl.productsList.add(product2);
+        productImpl.productsList.add(product3);
+        productImpl.productsList.add(product4);
+        productImpl.productsList.add(product5);
+
+        System.out.println(productImpl.addProduct(new Product(1, "Closet")));
+        System.out.println(productImpl.addProduct(new Product(6, "Closet")));
+        System.out.println(productImpl.productsList);
+
+        System.out.println(productImpl.deleteProduct(new Product(1, "Closet")));
+        System.out.println(productImpl.deleteProduct(product4));
+        System.out.println(productImpl.productsList);
+
+        System.out.println(productImpl.getName(3));
+        System.out.println(productImpl.getName(15));
+
+        System.out.println(productImpl.findByName("chair"));
+        System.out.println(productImpl.findByName("stool"));
     }
 }
---
+++ src/ProductImpl.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductImpl {
+    List<Product> productsList = new ArrayList<>();
+
+    public boolean addProduct(Product product) {
+        for (Product productInTheList : productsList) {
+            if (productInTheList.getId() == product.getId()) {
+                return false;
+            }
+        }
+        productsList.add(product);
+        return true;
+    }
+
+    public boolean deleteProduct(Product product) {
+        for (Product productInTheList : productsList) {
+            if (productInTheList.getId() == product.getId()) {
+                productsList.remove(product);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getName(int id) {
+        for (Product productInTheList : productsList) {
+            if (productInTheList.getId() == id) {
+                return productInTheList.getName();
+            }
+        }
+        return "";
+    }
+
+    public List<Integer> findByName(String name) {
+        List<Integer> result = new ArrayList<>();
+        for (Product productInTheList : productsList) {
+            if (productInTheList.getName().equalsIgnoreCase(name)) {
+                result.add(productInTheList.getId());
+            }
+        }
+        return result;
+    }
+
+}
